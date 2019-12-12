03a53d16ae09ab3849273b8a953fd6f016d3fb4f
m.kulikova <m.kulikova@itransition.com>
2019-12-12 17:10:58 +0300

    Change port

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
